c35cc2127d881b00d65ba08ed594ca41606c1eec
arthur <gogoke1@naver.com>
2018-12-22 02:24:22 +0900

    add PersonValidator Class & Controller binding

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 import com.example.demo.domain.Person;
 import com.example.demo.request.PersonRequest;
+import com.example.demo.validator.PersonValidator;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,13 +15,16 @@ import javax.validation.Valid;
 public class Controller {
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
+    @ResponseStatus(code = HttpStatus.CREATED)
     public PersonRequest getPerson(@RequestBody @Valid PersonRequest personRequest, BindingResult bindingResult){
 
+        new PersonValidator().validate(personRequest, bindingResult);
         if(bindingResult.hasErrors()){
             bindingResult.getAllErrors()
                     .stream()
-                    .map(s -> s.getDefaultMessage())
-                    .forEach(s -> log.error("{}", s));
+                    .forEach(s -> {
+                        log.error("[{}] {}", s.getCode(), s.getDefaultMessage());
+                    });
 
             return null;
         }
--- src/main/java/com/example/demo/validator/PersonValidator.java
+++ src/main/java/com/example/demo/validator/PersonValidator.java
@@ -1,6 +1,21 @@
 package com.example.demo.validator;
 
-import javax.validation.Validator;
+import com.example.demo.request.PersonRequest;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 
-public class PersonValidator  {
+public class PersonValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return PersonRequest.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        PersonRequest personRequest = (PersonRequest) target;
+
+        // TODO: 유효성검사
+
+        //errors.reject("arthur", "이곳에걸렷구나");
+    }
 }
