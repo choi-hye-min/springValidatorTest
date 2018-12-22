2913e24aed7f805446d09ee34758636ddb260830
arthur <gogoke1@naver.com>
2018-12-22 18:47:50 +0900

    junit5 추가

---
+++ src/test/java/com/example/demo/PersonApiTest.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@RunWith(JUnitPlatform.class)
+public class PersonApiTest {
+}
